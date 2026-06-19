c0b33b33d2c3bdda2420af5c404cab7bfee67c67
Yunying Hu <huyunying128@gmail.com>
2026-06-19 20:51:27 +0800

    Update __init__.py

--- src/fpwc/__init__.py
+++ src/fpwc/__init__.py
@@ -1,1 +1,22 @@
+"""Fuzzy Partition-Weighted Classification utilities."""
+
+from .distances import beta_distance_matrix
+from .memberships import (
+    aggregation_weights,
+    crisp_membership,
+    crisp_membership_from_distances,
+    fuzzy_membership,
+    fuzzy_membership_from_distances,
+)
+
+__all__ = [
+    "beta_distance_matrix",
+    "aggregation_weights",
+    "crisp_membership",
+    "crisp_membership_from_distances",
+    "fuzzy_membership",
+    "fuzzy_membership_from_distances",
+]
+
+__version__ = "0.1.0"
 
